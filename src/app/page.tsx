869f7da7784ba126817e0f4714cf8abfdbf84e06
Matthew Powell <matt.powell2784@gmail.com>
2023-07-23 09:56:26 +0100

    style: add nav bar components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <p>Banana Store</p>
+    <main className="flex min-h-screen min-w-screen">
+      <header>
+        <Image
+          src="/foot_boot_logo.svg"
+          alt="Footboot Logo"
+          width={200}
+          height={40}
+          className="p-2"
+        />
+        <nav>
+          <ul>
+            <li>
+              <a href="/store">Store</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </main>
   )
 }
