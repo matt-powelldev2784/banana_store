3245cd2a0318449861f3916de414564afd796432
Matthew Powell <matt.powell2784@gmail.com>
2023-07-23 19:10:39 +0100

    style: add icons and style nav bar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,21 +9,57 @@ export default function Home() {
           alt="Footboot Logo"
           width={200}
           height={40}
-          className="p-2"
+          className="m-3 mx-8"
         />
         <nav>
-          <ul className="flex gap-2 items-center justify-center px-4 h-full">
+          <ul className="flex gap-8 items-center justify-center mx-8 h-full">
             <li>
-              <a href="/store">Store</a>
+              <a href="/contact" className="flexRow gap-1 text-lg">
+                <Image
+                  src="/icons/boot.svg"
+                  alt="Footboot Boot Icon"
+                  width={30}
+                  height={30}
+                  className=""
+                />
+                Store
+              </a>
             </li>
             <li>
-              <a href="/about">Login</a>
+              <a href="/contact" className="flexRow gap-1 text-lg">
+                <Image
+                  src="/icons/signin.svg"
+                  alt="Login Icon"
+                  width={30}
+                  height={30}
+                  className=""
+                />
+                Login
+              </a>
             </li>
             <li>
-              <a href="/contact">Account</a>
+              <a href="/contact" className="flexRow gap-1 text-lg">
+                <Image
+                  src="/icons/person.svg"
+                  alt="Person Icon"
+                  width={30}
+                  height={30}
+                  className=""
+                />
+                Account
+              </a>
             </li>
             <li>
-              <a href="/contact">Cart</a>
+              <a href="/contact" className="flexRow gap-1 text-lg">
+                <Image
+                  src="/icons/basket.svg"
+                  alt="Basket Icon"
+                  width={30}
+                  height={30}
+                  className=""
+                />
+                Basket
+              </a>
             </li>
           </ul>
         </nav>
