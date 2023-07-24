4fa6887898ff98ca20516eca1e9d2b89ecbd1e27
Matthew Powell <matt.powell2784@gmail.com>
2023-07-24 17:46:24 +0100

    build: setup next auth and prisma

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,16 @@
+import NextAuth from 'next-auth'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { prisma } from '../../../../../prisma/db/client'
+import GithubProvider from 'next-auth/providers/github'
+
+const handler = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+  ],
+})
+
+export { handler as GET, handler as POST }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,10 @@
+'use client'
+import { useSession } from 'next-auth/react'
+
 export default function Home() {
+  const { data: session } = useSession()
+  console.log('session', session)
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
       <p>Banana Store</p>
