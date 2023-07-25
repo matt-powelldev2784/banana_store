56c615b02eefb9bdece2d631f7850f2d4c2f173c
Matthew Powell <matt.powell2784@gmail.com>
2023-07-25 18:42:49 +0100

    style: repsonsive layout added

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -6,16 +6,23 @@ import Image from 'next/image'
 
 export const Hero = () => {
   const SlidesJsx = slideImages.map(({ key, image, alt, text }) => (
-    <div key={key} className="slide-container w-full h-[620px] relative">
+    <div
+      key={key}
+      className="slide-container w-full h-[250px] md:h-[500px] lg:h-[620px] relative"
+    >
       <Image
         src={`/slideImages/${image}`}
         alt={alt}
         fill
         style={{ objectFit: 'cover' }}
       />
-      <div className="absolute flex flex-col justify-center items-start left-32 max-w-md h-full">
-        <p className=" text-white font-bold text-7xl ">{text}</p>
-        <button className="bg-white px-4 py-2 rounded my-8">Shop Now</button>
+      <div className="absolute flex flex-col justify-center items-center lg:items-start lg:left-32 lg:max-w-md h-full w-full">
+        <p className="text-center lg:text-left font-bold text-3xl md:text-5xl lg:text-7xl text-white mx-8 md:mx-60 lg:mx-0">
+          {text}
+        </p>
+        <button className="bg-white px-4 py-2 rounded my-4 md:my-8">
+          Shop Now
+        </button>
       </div>
     </div>
   ))
