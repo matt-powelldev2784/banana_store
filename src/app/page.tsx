7c975efcac38d1362a15a61b117d504463100055
Matthew Powell <matt.powell2784@gmail.com>
2023-07-25 13:50:15 +0100

    feat: add homepage nav bar and hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,98 @@
 'use client'
 import { useSession } from 'next-auth/react'
+import Image from 'next/image'
 
 export default function Home() {
   const { data: session } = useSession()
   console.log('session', session)
 
-  if (session) {
-    return <div>{session?.user?.name} signed in</div>
-  }
+  // if (session) {
+  //   return <div>{session?.user?.name} signed in</div>
+  // }
 
-  if (!session) {
-    return <div>not signed in</div>
-  }
+  // if (!session) {
+  //   return <div>not signed in</div>
+  // }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <p>Banana Store</p>
+    <main className="min-h-screen min-w-screen">
+      <header className="flex flex-row justify-between h-fit w-full my-2">
+        <Image
+          src="/foot_boot_logo.svg"
+          alt="Footboot Logo"
+          width={200}
+          height={40}
+          className="m-3 mx-8"
+        />
+        <nav>
+          <ul className="flex gap-8 items-center justify-center mx-8 h-full">
+            <li>
+              <a href="/contact" className="flexRow gap-1 text-lg">
+                <Image
+                  src="/icons/boot.svg"
+                  alt="Footboot Boot Icon"
+                  width={30}
+                  height={30}
+                  className=""
+                />
+                Store
+              </a>
+            </li>
+            <li>
+              <a href="/contact" className="flexRow gap-1 text-lg">
+                <Image
+                  src="/icons/signin.svg"
+                  alt="Login Icon"
+                  width={30}
+                  height={30}
+                  className=""
+                />
+                Login
+              </a>
+            </li>
+            <li>
+              <a href="/contact" className="flexRow gap-1 text-lg">
+                <Image
+                  src="/icons/person.svg"
+                  alt="Person Icon"
+                  width={30}
+                  height={30}
+                  className=""
+                />
+                Account
+              </a>
+            </li>
+            <li>
+              <a href="/contact" className="flexRow gap-1 text-lg">
+                <Image
+                  src="/icons/basket.svg"
+                  alt="Basket Icon"
+                  width={30}
+                  height={30}
+                  className=""
+                />
+                Basket
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <section className="w-screen h-[600px] relative">
+        <Image
+          src="/ronaldo.jpg"
+          alt="Football Boot"
+          fill
+          style={{ objectFit: 'cover' }}
+          className=""
+        />
+        {/* <Image
+          src="/adidas_black_white.webp"
+          alt="Football Boot"
+          fill
+          style={{ objectFit: 'cover' }}
+          className=""
+        /> */}
+      </section>
     </main>
   )
 }
