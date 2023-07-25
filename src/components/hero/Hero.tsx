24376f70bc893b43be764a65b5bd08f7670337b7
Matthew Powell <matt.powell2784@gmail.com>
2023-07-25 16:43:42 +0100

    feat: add image slider logic

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -1,15 +1,34 @@
-import Image from 'next/image'
+import React from 'react'
+import { Slide } from 'react-slideshow-image'
+import 'react-slideshow-image/dist/styles.css'
+import { slideImages } from './data/slideData'
+
+const spanStyle = {
+  padding: '20px',
+  background: '#efefef',
+  color: '#000000',
+}
+
+const divStyle = {
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  backgroundSize: 'cover',
+  height: '400px',
+}
 
 export const Hero = () => {
+  const slidesJsx = slideImages.map(({ key, image, text }) => (
+    <div key={key}>
+      <div style={{ ...divStyle, backgroundImage: `url(${image})` }}>
+        <span style={spanStyle}>{text}</span>
+      </div>
+    </div>
+  ))
+
   return (
-    <section className="w-screen h-[600px] relative">
-      <Image
-        src="/ronaldo.jpg"
-        alt="Football Boot"
-        fill
-        style={{ objectFit: 'cover' }}
-        className=""
-      />
-    </section>
+    <div className="slide-container">
+      <Slide>{slidesJsx}</Slide>
+    </div>
   )
 }
---
+++ src/components/hero/data/slideData.ts
@@ -0,0 +1,22 @@
+export const slideImages = [
+  {
+    key: 0,
+    image: 'ronaldo.jpg',
+    text: 'New Ronaldo CR7 Collection now in stock',
+  },
+  {
+    key: 1,
+    image: 'adidas_black_white.webp',
+    text: 'New adidas collection now in stock',
+  },
+  {
+    key: 2,
+    image: 'boot_blue_bg2.webp',
+    text: 'Sale. 20% off all boots',
+  },
+]
+
+export const slideProperties = {
+  prevArrow: null,
+  nextArrow: null,
+}
