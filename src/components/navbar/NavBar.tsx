a60e89346a2ee674bd6938a482084f6b11024516
Matthew Powell <matt.powell2784@gmail.com>
2023-07-25 19:45:02 +0100

    style: add resposive layout

--- src/components/navbar/NavBar.tsx
+++ src/components/navbar/NavBar.tsx
@@ -3,16 +3,31 @@ import { NavItems } from './components/NavItems'
 
 export const NavBar = () => {
   return (
-    <header className="flex flex-row justify-between h-fit w-full my-2">
-      <Image
-        src="/foot_boot_logo.svg"
-        alt="Footboot Logo"
-        width={200}
-        height={40}
-        className="m-3 mx-8"
-      />
-      <nav>
-        <ul className="flex gap-8 items-center justify-center mx-8 h-full">
+    <header className="">
+      <nav className="flex flex-row justify-between items-center h-fit w-full my-2">
+        <Image
+          src={`/icons/hamburger.svg`}
+          alt={'Hamburger Menu Icon'}
+          width={30}
+          height={30}
+          className="lg:hidden ml-2"
+        />
+        <Image
+          src="/foot_boot_logo.svg"
+          alt="Footboot Logo"
+          width={200}
+          height={40}
+          className="sm:h-6 lg:m-3 lg:mx-8 m-0"
+        />
+        <Image
+          src={`/icons/signin.svg`}
+          alt={'Hamburger Menu Icon'}
+          width={30}
+          height={30}
+          className="lg:hidden mr-2"
+        />
+
+        <ul className="hidden lg:flex gap-8 items-center justify-center mx-8 h-full">
           <NavItems />
         </ul>
       </nav>
